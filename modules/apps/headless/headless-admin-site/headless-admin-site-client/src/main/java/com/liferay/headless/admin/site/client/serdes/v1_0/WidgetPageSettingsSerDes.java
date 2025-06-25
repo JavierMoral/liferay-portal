4a738ce70c3105a4362bf951cc2a2967fc3819be
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-06-25 11:24:00 +0200

    LPD-57816 BuildRest

--- modules/apps/headless/headless-admin-site/headless-admin-site-client/src/main/java/com/liferay/headless/admin/site/client/serdes/v1_0/WidgetPageSettingsSerDes.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-client/src/main/java/com/liferay/headless/admin/site/client/serdes/v1_0/WidgetPageSettingsSerDes.java
@@ -152,15 +152,15 @@ public class WidgetPageSettingsSerDes {
 			sb.append(widgetPageSettings.getHiddenFromNavigation());
 		}
 
-		if (widgetPageSettings.getNavigationMenuSettings() != null) {
+		if (widgetPageSettings.getNavigationSettings() != null) {
 			if (sb.length() > 1) {
 				sb.append(", ");
 			}
 
-			sb.append("\"navigationMenuSettings\": ");
+			sb.append("\"navigationSettings\": ");
 
 			sb.append(
-				String.valueOf(widgetPageSettings.getNavigationMenuSettings()));
+				String.valueOf(widgetPageSettings.getNavigationSettings()));
 		}
 
 		if (widgetPageSettings.getOpenGraphSettings() != null) {
@@ -283,13 +283,13 @@ public class WidgetPageSettingsSerDes {
 				String.valueOf(widgetPageSettings.getHiddenFromNavigation()));
 		}
 
-		if (widgetPageSettings.getNavigationMenuSettings() == null) {
-			map.put("navigationMenuSettings", null);
+		if (widgetPageSettings.getNavigationSettings() == null) {
+			map.put("navigationSettings", null);
 		}
 		else {
 			map.put(
-				"navigationMenuSettings",
-				String.valueOf(widgetPageSettings.getNavigationMenuSettings()));
+				"navigationSettings",
+				String.valueOf(widgetPageSettings.getNavigationSettings()));
 		}
 
 		if (widgetPageSettings.getOpenGraphSettings() == null) {
@@ -363,7 +363,7 @@ public class WidgetPageSettingsSerDes {
 				return false;
 			}
 			else if (Objects.equals(
-						jsonParserFieldName, "navigationMenuSettings")) {
+						jsonParserFieldName, "navigationSettings")) {
 
 				return false;
 			}
@@ -445,11 +445,11 @@ public class WidgetPageSettingsSerDes {
 				}
 			}
 			else if (Objects.equals(
-						jsonParserFieldName, "navigationMenuSettings")) {
+						jsonParserFieldName, "navigationSettings")) {
 
 				if (jsonParserFieldValue != null) {
-					widgetPageSettings.setNavigationMenuSettings(
-						NavigationMenuSettingsSerDes.toDTO(
+					widgetPageSettings.setNavigationSettings(
+						NavigationSettingsSerDes.toDTO(
 							(String)jsonParserFieldValue));
 				}
 			}
