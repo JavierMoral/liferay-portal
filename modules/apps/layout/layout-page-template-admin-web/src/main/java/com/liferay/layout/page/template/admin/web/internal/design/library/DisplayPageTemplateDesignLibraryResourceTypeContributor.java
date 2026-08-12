4c18168481c91ed731fa9d2f4953512a83f06384
Javier Moral <javier.moral@liferay.com>
2026-08-12 17:52:45 +0200

    LPD-81651 Register display page templates as a Design Library resource type
    Display page templates now appear in the Design Library Admin listing with
    their own label, icon and row actions, contributed from the module that owns
    the type rather than hardcoded into the Design Library Admin.
    
    The type is left unset even though display page templates share an entry class
    name with masters and page templates: the client matches a declared type
    against a type field the search API does not return yet, and the guard in
    LayoutPageTemplateEntryLocalServiceImpl only admits display page templates in a
    Design Library, so the entry class name identifies them on its own.

---
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/design/library/DisplayPageTemplateDesignLibraryResourceTypeContributor.java
@@ -0,0 +1,275 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.layout.page.template.admin.web.internal.design.library;
+
+import com.liferay.depot.model.DepotEntry;
+import com.liferay.design.library.resource.type.DesignLibraryResourceCreationItem;
+import com.liferay.design.library.resource.type.DesignLibraryResourceTypeContributor;
+import com.liferay.frontend.data.set.model.FDSActionDropdownItem;
+import com.liferay.info.item.InfoItemClassDetails;
+import com.liferay.info.item.InfoItemFormVariation;
+import com.liferay.info.item.InfoItemServiceRegistry;
+import com.liferay.info.item.provider.InfoItemFormVariationsProvider;
+import com.liferay.info.localized.InfoLocalizedValue;
+import com.liferay.info.permission.provider.InfoPermissionProvider;
+import com.liferay.layout.page.template.admin.constants.LayoutPageTemplateAdminPortletKeys;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateActionKeys;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateConstants;
+import com.liferay.layout.page.template.info.item.capability.DisplayPageInfoItemCapability;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import jakarta.portlet.PortletRequest;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Javier Moral
+ */
+@Component(
+	property = "service.ranking:Integer=300",
+	service = DesignLibraryResourceTypeContributor.class
+)
+public class DisplayPageTemplateDesignLibraryResourceTypeContributor
+	implements DesignLibraryResourceTypeContributor {
+
+	@Override
+	public String getColor() {
+		return "blue";
+	}
+
+	@Override
+	public List<DesignLibraryResourceCreationItem> getCreationItems(
+			HttpServletRequest httpServletRequest, DepotEntry depotEntry,
+			String backURL)
+		throws PortalException {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		Group depotGroup = depotEntry.getGroup();
+
+		return Collections.singletonList(
+			new DesignLibraryResourceCreationItem(
+				"add-display-page-template",
+				LanguageUtil.get(
+					httpServletRequest, "new-display-page-template"),
+				"{DesignLibraryAddDisplayPageTemplate} from " +
+					"layout-page-template-admin-web",
+				HashMapBuilder.<String, Object>put(
+					"formSubmitURL",
+					PortletURLBuilder.create(
+						PortalUtil.getControlPanelPortletURL(
+							httpServletRequest, depotGroup,
+							LayoutPageTemplateAdminPortletKeys.
+								LAYOUT_PAGE_TEMPLATES,
+							0, 0, PortletRequest.ACTION_PHASE)
+					).setActionName(
+						"/layout_page_template_admin/add_display_page"
+					).setRedirect(
+						backURL
+					).buildString()
+				).put(
+					"mappingTypes",
+					_getMappingTypesJSONArray(depotGroup, themeDisplay)
+				).put(
+					"namespace",
+					PortalUtil.getPortletNamespace(
+						LayoutPageTemplateAdminPortletKeys.
+							LAYOUT_PAGE_TEMPLATES)
+				).build()));
+	}
+
+	@Override
+	public String getDefaultActionId() {
+		return "edit";
+	}
+
+	@Override
+	public String getEntryClassName() {
+		return LayoutPageTemplateEntry.class.getName();
+	}
+
+	@Override
+	public List<FDSActionDropdownItem> getFDSActionDropdownItems(
+			HttpServletRequest httpServletRequest, DepotEntry depotEntry,
+			String backURL)
+		throws PortalException {
+
+		Group depotGroup = depotEntry.getGroup();
+
+		return ListUtil.fromArray(
+			new FDSActionDropdownItem(
+				PortletURLBuilder.create(
+					PortalUtil.getControlPanelPortletURL(
+						httpServletRequest, depotGroup,
+						LayoutPageTemplateAdminPortletKeys.
+							LAYOUT_PAGE_TEMPLATES,
+						0, 0, PortletRequest.RENDER_PHASE)
+				).setMVCRenderCommandName(
+					"/layout_page_template_admin/edit_display_page"
+				).setRedirect(
+					backURL
+				).setParameter(
+					"displayPageTemplateExternalReferenceCode",
+					"{embedded.externalReferenceCode}"
+				).buildString(),
+				"pencil", "edit", LanguageUtil.get(httpServletRequest, "edit"),
+				null, null, "link"),
+			new FDSActionDropdownItem(
+				StringBundler.concat(
+					"/o/headless-admin-site/v1.0/design-libraries/",
+					depotGroup.getExternalReferenceCode(),
+					"/display-page-templates/{embedded.externalReferenceCode}"),
+				"trash", "delete",
+				LanguageUtil.get(httpServletRequest, "delete"), "delete",
+				"delete", "async"));
+	}
+
+	@Override
+	public String getIcon() {
+		return "page";
+	}
+
+	@Override
+	public String getKey() {
+		return "display-page-template";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(locale, "display-page-template");
+	}
+
+	@Override
+	public boolean hasAddPermission(
+		PermissionChecker permissionChecker, DepotEntry depotEntry) {
+
+		return _portletResourcePermission.contains(
+			permissionChecker, depotEntry.getGroupId(),
+			LayoutPageTemplateActionKeys.ADD_LAYOUT_PAGE_TEMPLATE_ENTRY);
+	}
+
+	@Override
+	public boolean hasViewPermission(
+		PermissionChecker permissionChecker, DepotEntry depotEntry) {
+
+		return hasAddPermission(permissionChecker, depotEntry);
+	}
+
+	private JSONArray _getMappingTypesJSONArray(
+		Group depotGroup, ThemeDisplay themeDisplay) {
+
+		JSONArray jsonArray = _jsonFactory.createJSONArray();
+
+		for (InfoItemClassDetails infoItemClassDetails :
+				_infoItemServiceRegistry.getInfoItemClassDetails(
+					depotGroup.getGroupId(), DisplayPageInfoItemCapability.KEY,
+					themeDisplay.getPermissionChecker())) {
+
+			jsonArray.put(
+				JSONUtil.put(
+					"id",
+					String.valueOf(
+						PortalUtil.getClassNameId(
+							infoItemClassDetails.getClassName()))
+				).put(
+					"label",
+					infoItemClassDetails.getLabel(themeDisplay.getLocale())
+				).put(
+					"subtypes",
+					_getSubtypesJSONArray(
+						depotGroup, infoItemClassDetails, themeDisplay)
+				));
+		}
+
+		return jsonArray;
+	}
+
+	private JSONArray _getSubtypesJSONArray(
+		Group depotGroup, InfoItemClassDetails infoItemClassDetails,
+		ThemeDisplay themeDisplay) {
+
+		JSONArray jsonArray = _jsonFactory.createJSONArray();
+
+		InfoItemFormVariationsProvider<?> infoItemFormVariationsProvider =
+			_infoItemServiceRegistry.getFirstInfoItemService(
+				InfoItemFormVariationsProvider.class,
+				infoItemClassDetails.getClassName());
+
+		if (infoItemFormVariationsProvider == null) {
+			return jsonArray;
+		}
+
+		InfoPermissionProvider infoPermissionProvider =
+			_infoItemServiceRegistry.getFirstInfoItemService(
+				InfoPermissionProvider.class,
+				infoItemClassDetails.getClassName());
+
+		for (InfoItemFormVariation infoItemFormVariation :
+				infoItemFormVariationsProvider.getInfoItemFormVariations(
+					depotGroup.getGroupId())) {
+
+			if ((infoPermissionProvider != null) &&
+				!infoPermissionProvider.hasViewPermission(
+					infoItemFormVariation.getKey(), depotGroup.getGroupId(),
+					themeDisplay.getPermissionChecker())) {
+
+				continue;
+			}
+
+			jsonArray.put(
+				JSONUtil.put(
+					"id", String.valueOf(infoItemFormVariation.getKey())
+				).put(
+					"label",
+					() -> {
+						InfoLocalizedValue<String> labelInfoLocalizedValue =
+							infoItemFormVariation.getLabelInfoLocalizedValue();
+
+						return labelInfoLocalizedValue.getValue(
+							themeDisplay.getLocale());
+					}
+				));
+		}
+
+		return jsonArray;
+	}
+
+	@Reference
+	private InfoItemServiceRegistry _infoItemServiceRegistry;
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+	@Reference(
+		target = "(resource.name=" + LayoutPageTemplateConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+}
